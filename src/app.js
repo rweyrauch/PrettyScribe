a5efee7a60aacc5d3bc57b0f74cb0d481415d877
Rick Weyrauch <rick.weyrauch@base2s.com>
2020-01-22 15:32:35 -0800

    Use DOM to parse XML roster file.

--- src/app.js
+++ src/app.js
@@ -1,38 +1,84 @@
-function handleFileSelect(event: Event) {
-  const input = event.target as HTMLInputElement;
-  const files: FileList | null = input.files;
+function handleFileSelect(event) {
+  const input = event.target;
+  const files = input.files;
 
   if (files) {
     // files is a FileList of File objects. List some properties.
     var output = [];
     for (let f of files) {
       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                f.size, ' bytes, last modified: ',
-                f.lastModified ? f.lastModified.toString() : 'n/a',
-                '</li>');
+        f.size, ' bytes, last modified: ',
+        f.lastModified ? f.lastModified.toString() : 'n/a',
+        '</li>');
       const reader = new FileReader();
-      reader.onload = function(e: Event) {
-        const re = e.target as FileReader;
+      reader.onload = function (e) {
+        const re = e.target;
         if (re && re.result) {
           //console.log(re.result);
           if (re.result) {
             // Skip encoding tag
-            const xmldatastart = re.result.toString().indexOf(',')+1;
+            const xmldatastart = re.result.toString().indexOf(',') + 1;
             console.log("XML Start: " + xmldatastart);
-            const xmldata = re.result.toString().slice(xmldatastart);
+            const xmldata = window.atob(re.result.toString().slice(xmldatastart));
             createRoster(xmldata);
           }
         }
       }
+      reader.readAsDataURL(f);
     }
-    const list: HTMLElement|null = document.getElementById('list');
+    const list = document.getElementById('list');
     if (list) list.innerHTML = '<ul>' + output.join('') + '</ul>';
   }
 }
 
-const finput: HTMLElement|null = document.getElementById('input-file');
+const finput = document.getElementById('roster-file');
 if (finput) finput.addEventListener('change', handleFileSelect, false);
 
-function createRoster(str: string): void {
+function createRoster(str) {
+
+  var pts = 0;
+  var pl = 0;
+  var cp = 0;
+
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(str, "text/xml");
+  if (doc) {
+
+    var costs = doc.querySelectorAll("roster>costs>cost");
+    for (cost of costs) {
+      if (cost.hasAttribute("name") && cost.hasAttribute("value")) {
+        let which = cost.getAttributeNode("name").nodeValue;
+        let value = cost.getAttributeNode("value").nodeValue;
+        if (which == " PL") {
+           pl = +value;
+        }
+        else if (which === "pts") {
+           pts = +value;
+        }
+        else if (which === "CP") {
+          cp = +value;
+        }
+      }
+    }
+
+    var forceList = [];
+
+    var forces = doc.querySelectorAll("roster>forces>force");
+    for (force of forces) {
+         if (force.hasAttribute("name") && force.hasAttribute("catalogueName")) {
+          let which = force.getAttributeNode("name").nodeValue;
+          let value = force.getAttributeNode("catalogueName").nodeValue;
+
+          forceList.push({type: which, catalogue: value});
+
+          var rules = document.querySelectorAll("roster>forces>force>rules>rule");
+          console.log("Name: " + which + "  Rules: " + rules);
+      }
+    }
+
+    console.log(forceList);
+  }
+
+  console.log("Points: " + pts + "  Power Level: " + pl + "  CP: " + cp);
 
 }
--- src/roster.js
+++ src/roster.js
@@ -1,11 +1,11 @@
 
 export class Roster {
-    _polwerLevel: number = 0;
-    _commandPoints: number = 0;
-    _points: number = 0;
+    _powerLevel = 0;
+    _commandPoints= 0;
+    _points = 0;
 };
  
-export function createRoster(xml: string): Roster| null {
+export function createRoster(xml) {
     if (!xml) return null;
 
     return new Roster();
