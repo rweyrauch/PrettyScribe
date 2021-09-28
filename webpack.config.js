abfd130fda839c6ae7dda03646e695edbc5ddd74
Rick Weyrauch <rpweyrauch@gmail.com>
2021-09-28 12:48:00 -0700

    Update webpack-cli.

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,10 @@ module.exports = {
             '.ts', '.js'
         ],
         fallback: { "stream": false }
+    },
+    devServer: {
+        static: {
+            directory: __dirname,
+        },
     }
 };
