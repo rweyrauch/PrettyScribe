68e8756c3c7c1e1a341782ed0fad67f8308eec1a
mardanbeigi <mardanbeigi@gmail.com>
2024-02-27 21:58:17 -0800

    Tool for pretty-printing rosterizer registry files

---
+++ src/rosterizer.ts
@@ -0,0 +1,42 @@
+/*
+ * Generic types and util functions for dealing with Rosterizer registries.
+ */
+
+export interface Entry {
+  assets: Assets;
+  designation: string; // Name
+  stats: Stats;
+  classification: string; // Type
+  tally: { [key: string]: number; };
+  text: string;
+  keywords: { [key: string]: string[]; };
+}
+
+export interface Assets {
+  included: Entry[];
+  traits: Entry[];
+}
+
+export type Stats = { [key: string]: StatValue; };
+
+export interface StatValue {
+  group: string;
+  statOrder: number;
+  groupOrder?: number;
+  visibility: string;
+  format?: string;
+  value: string | number;
+}
+
+/**
+ * Given generic Stats, will return an ordered list of key-value pairs that are
+ * relevant (eg for a unit or a weapon).
+ */
+export function filterAndOrderStats(stats: Stats) {
+  return Object.entries(stats)
+    .filter(s => s[1].statOrder >= 0
+            && s[1].groupOrder === undefined
+            && s[1].visibility === 'normal')
+    .sort((a, b) => a[1].statOrder - b[1].statOrder);
+}
+
